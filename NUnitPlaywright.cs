076460e94bebb775695f1f5fbbe7a6bea290c21c
Hamid Baydamirov <baydamirovv35@gmail.com>
2025-07-03 21:35:26 +0400

    Initial Playwright .NET commit

---
+++ NUnitPlaywright.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightDemo;
+
+public class NUnitPlaywright: PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://eaapp.somee.com/");
+
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        await Page.ClickAsync("text=Login");
+        await Page.FillAsync("#UserName", "admin");
+        await Page.FillAsync("#Password", "password");
+        await Page.ClickAsync("text=Login");
+        await Expect(Page.Locator("text = Employee List")).ToBeVisibleAsync();
+    }
+}
