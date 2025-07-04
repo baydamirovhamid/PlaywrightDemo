9dd4de972f695a807b825082f2c5c7989f5363ec
Hamid Baydamirov <baydamirovv35@gmail.com>
2025-07-05 01:16:55 +0400

    Playwright test ve POM güncellemeleri

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+
+namespace PlaywrightDemo.Pages;
+    public class LoginPage { 
+
+    private IPage _page;
+    private readonly ILocator _lnkLogin;
+    private readonly ILocator _txtUserName;
+    private readonly ILocator _txtPassword;
+    private readonly ILocator _btnLogin;
+    private readonly ILocator _lnkEmployeeList;
+
+
+    public LoginPage(IPage page)
+    {
+        _page = page;
+        _lnkLogin = _page.Locator("text=Login");
+        _txtUserName = _page.Locator("#UserName");
+        _txtPassword = _page.Locator("#Password");
+        _btnLogin = _page.Locator("text = Log in");
+        _lnkEmployeeList = _page.Locator("text = Employee List");
+    }
+
+    public async Task ClickLogin()
+    {
+        await _lnkLogin.ClickAsync();
+    }
+
+    public async Task Login(string userName, string password)
+    {
+        await _txtUserName.FillAsync(userName);
+        await _txtPassword.FillAsync(password);
+        await _btnLogin.ClickAsync();
+    }
+
+    public async Task<bool> IsEmployeeListExist()
+    {
+        var isExist = await _lnkEmployeeList.IsVisibleAsync();
+        Assert.That(isExist, Is.True);
+        return isExist;
+    }
+
+
+
+}
---
+++ Pages/LoginPageUpgraded.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+
+namespace PlaywrightDemo.Pages;
+public class LoginPageUpgraded
+{
+
+    private IPage _page;
+    public LoginPageUpgraded(IPage page) => _page = page;
+
+    private ILocator _lnkLogin => _page.Locator("text=Login");
+    private ILocator _txtUserName => _page.Locator("#UserName");
+    private ILocator _txtPassword => _page.Locator("#Password");
+    private ILocator _btnLogin => _page.Locator("text=Log in");
+    private ILocator _lnkEmployeeDetails => _page.Locator("text= Employee Details");
+    private ILocator _lnkEmployeeLists => _page.Locator("text= Employee List");
+
+    [Obsolete]
+    public async Task ClickLogin() 
+    {
+        await _page.RunAndWaitForNavigationAsync(async () =>
+        {
+            await _lnkLogin.ClickAsync();
+        }, new PageRunAndWaitForNavigationOptions
+        {
+            UrlString = "**/Login"
+        });
+    }
+
+    public async Task ClickEmployeeList()
+    {
+        await _lnkEmployeeLists.ClickAsync();
+    }
+
+    public async Task Login(string userName, string password)
+    {
+        await _txtUserName.FillAsync(userName);
+        await _txtPassword.FillAsync(password);
+        await _btnLogin.ClickAsync();
+    }
+
+    public async Task<bool> IsEmployeeDetailsExist()
+    {
+        var isExist = await _lnkEmployeeLists.IsVisibleAsync();
+        Assert.That(isExist, Is.True);
+        return isExist;
+    }
+}
